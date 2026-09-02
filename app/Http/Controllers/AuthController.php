8adff701270cca3c8b4598a9dc7ef1634c490494
kaizen <agenciakaizendesign@gmail.com>
2026-09-02 19:58:22 -0300

    security: remover endpoint legado de cadastro

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -26,27 +26,6 @@ class AuthController extends Controller
         return response()->json(['user' => Auth::user()]);
     }
 
-    public function register(Request $request): JsonResponse
-    {
-        $data = $request->validate([
-            'name' => ['required', 'string', 'max:120'],
-            'email' => ['required', 'email', 'max:255', 'unique:users,email'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
-
-        $user = User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-            'role' => 'user',
-        ]);
-
-        Auth::login($user);
-        $request->session()->regenerate();
-
-        return response()->json(['user' => $user], 201);
-    }
-
     public function logout(Request $request): JsonResponse
     {
         Auth::logout();
